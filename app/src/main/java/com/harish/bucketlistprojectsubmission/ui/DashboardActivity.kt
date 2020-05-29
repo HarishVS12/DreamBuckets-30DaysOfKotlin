15cc879e293a097871b95893e90d44b6235e8f28
harish <harishv783@gmail.com>
2020-05-29 19:01:27 +0530

    Added core dependencies

--- app/src/main/java/com/harish/bucketlistprojectsubmission/ui/DashboardActivity.kt
+++ app/src/main/java/com/harish/bucketlistprojectsubmission/ui/DashboardActivity.kt
@@ -1,13 +1,15 @@
-package com.harish.bucketlistprojectsubmission
+package com.harish.bucketlistprojectsubmission.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.harish.bucketlistprojectsubmission.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
     }
 }
